f47a97e76ff22d0056a7c3e6ed23d0f48b30e657
Katie Hoesley <katiehoesley@gmail.com>
2018-11-06 21:27:28 -0800

    changes to files to render other components

--- server/index.js
+++ server/index.js
@@ -3,42 +3,60 @@ var app = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 
-var serverOne = 'http://localhost:3001',
-    serverTwo = 'http://localhost:3002',
-    serverThree = 'http://localhost:3002';
-    serverFour = 'http://localhost:3009'; //mine 
+console.log('hi')
+
+app.use(express.static(`${__dirname}/../public`));
+
+var ServerOne = 'http://localhost:3001',
+    ServerTwo = 'http://localhost:3009'
+    ServerThree = 'http://localhost:3004',
+    ServerFour = 'http://localhost:3000';
  
-app.all("/app1/*", function(req, res) { //not sure what to put at this endpoint, is it the endpoint from each specific app? 
-    console.log('redirecting to Server1');
+app.all('/stocks/1', function(req, res) {
+    console.log('redirecting to Server1 alan');
     apiProxy.web(req, res, {
-        target: serverOne
+        target: ServerOne
     });
 });
 
-app.all("/app2/*", function(req, res) {
-    console.log('redirecting to Server2');
+app.all('/companies/1', function(req, res) {
+    console.log('redirecting to Server1 alan');
     apiProxy.web(req, res, {
-        target: serverTwo
+        target: ServerOne
     });
 });
 
-app.all("/app3/*", function(req, res) {
-    console.log('redirecting to Server3');
+app.all('/companiesKatie/1', function(req, res) {
+    console.log('redirecting to Server2 katie');
     apiProxy.web(req, res, {
-        target: serverThree
+        target: ServerTwo
     });
 });
 
-app.all("/app4/*", function(req, res) {
-    console.log('redirecting to Server4');
+app.all('/company/1', function(req, res) {
+    console.log('redirecting to Server3 jim');
     apiProxy.web(req, res, {
-        target: serverFour
+        target: ServerThree
     });
 });
 
-const port = 3000; 
+
+app.all('/stocks/1', function(req, res) {
+    console.log('redirecting to Server3 jim');
+    apiProxy.web(req, res, {
+        target: ServerThree 
+    });
+});
+
+app.all('/companiesJim/1', function(req, res) {
+    console.log('redirecting to Server4 jonathan ');
+    apiProxy.web(req, res, {
+        target: ServerFour
+    });
+});
+
+const port = 3008; 
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
-  
